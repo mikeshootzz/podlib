081ee23458fc8d9da3143a167c2ad848c2f6dbe7
Mike <mike@ditton.ch>
2024-10-14 08:53:20 +0200

    initial script

---
+++ podlib.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+# Function: import_music
+import_music() {
+  echo "Importing music into the library..."
+  beet import
+}
+
+# Function: get_covers
+get_covers() {
+  echo "Getting covers for the entire music library..."
+  sacad_r ~/Music/Mike/ 500 cover.jpg
+}
+
+# Function: sync_ipod
+sync_ipod() {
+  echo "Syncing music to your iPod..."
+  Example: rsync -av --delete ~/Music/Mike/ /Volumes/MIKE\'S\ IPOD/Music/
+}
+
+# Function: download_music
+download_music() {
+  local service="$1"
+  local url="$2"
+
+  case "$service" in
+  # --spotify)
+  #   echo "Downloading from Spotify..."
+  #   # Use spotdl to download from Spotify
+  #   mkdir /tmp/musicdl
+  #   cd /tmp/musicdl
+  #   spotdl "$url" && beet import .
+  #   cd
+  #   rm -rf /tmp/musicdl
+  #   ;;
+  # --youtube)
+  #   echo "Downloading from YouTube..."
+  #   # Use spotdl to download from YouTube
+  #   # Example: spotdl "$url" -o /path/to/temp && beets import /path/to/temp && rm -rf /path/to/temp
+  #   ;;
+  --tidal)
+    echo "Downloading from Tidal..."
+    # Use Tidal Downloader NG or similar tool
+    tidal-dl-ng dl "$url" && beet import /Users/mike/Documents/Music && rm -rf /Users/mike/Documents/Music/* && sacad_r ~/Music/Mike/ 500 cover.jpg
+    ;;
+  *)
+    echo "Invalid download service specified. Use --spotify, --youtube, or --tidal."
+    ;;
+  esac
+}
+
+# Main Script Logic
+case "$1" in
+import)
+  import_music
+  ;;
+get-covers)
+  get_covers
+  ;;
+sync)
+  sync_ipod
+  ;;
+download)
+  if [[ -n "$2" && -n "$3" ]]; then
+    download_music "$2" "$3"
+  else
+    echo "Usage: $0 download --spotify|--youtube|--tidal <URL>"
+  fi
+  ;;
+*)
+  echo "Usage: $0 {import|get-covers|sync|download --spotify|--youtube|--tidal <URL>}"
+  ;;
+esac
