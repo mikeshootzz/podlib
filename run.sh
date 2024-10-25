3f4763e6e847eb29eb32488b7fd20e66f28a9e23
Mike <mike@ditton.ch>
2024-10-25 13:34:00 +0200

    outsourced the stuff

--- run.sh
+++ run.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-docker compose run --rm --remove-orphans podlib podlib "$@"
+
+# Check if the iPod is mounted by searching the mount list
+if mount | grep '/Volumes/IPOD' >/dev/null; then
+  # iPod is mounted; include the volume mount option
+  docker compose run --rm --remove-orphans -v /Volumes/IPOD/Music:/ipod podlib podlib "$@"
+else
+  # iPod is not mounted; run without the volume mount
+  echo "WARNING: Your iPod is not mounted or your mount path is incorrect! You wont be able to sync your music"
+  docker compose run --rm --remove-orphans podlib podlib "$@"
+fi
